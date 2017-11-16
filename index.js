2a79c04895f5cc610bea9fab5cddf12016173bdb
sunny-udhani <sunny.iv09@gmail.com>
2017-11-16 12:54:43 -0800

    working webhook :shipit:

--- index.js
+++ index.js
@@ -1,363 +1,128 @@
-'use strict'
+const express = require('express');
+const request = require('superagent');
+const bodyParser = require('body-parser');
+const https = require('https');
 
-const express = require('express')
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const request = require('request')
+
+//Variables
+let pageToken = "EAACk3taKgYIBAArgqQfGbw2X4CFjIoJYZCM7z0ZCLaI6z7Pd7V4Fs2Ru2joRF1QBoRHOoEwKuuZBQ06mrUJwXcRosL3HiyGkq6QDLmL4IYHlMAmqgLOxqYDcGIjYkSo1lyrDXaK1tS6KbCI0Y1ZCfRbwpb9MnQoDo0Ajh9R5jAZDZD";
+const verifyToken = "mohit"
+const privkey = "/etc/letsencrypt/live/cbot272.com/privkey.pem";
+const cert = "/etc/letsencrypt/live/cbot272.com/cert.pem";
+const chain = "/etc/letsencrypt/live/cbot272.com/fullchain.pem";
+
+
 const app = express();
+const fs = require('fs');
 
-var key = fs.readFileSync('./private.key');
-var cert = fs.readFileSync( './domain.crt' );
+app.use(bodyParser.json());
 
-var options = {
-  key: key,
-  cert: cert
-};
-
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.verify_token'] === verifyToken) {
+        return res.send(req.query['hub.challenge']);
     }
-    else{
-		res.send('Error, wrong token')
-	}
-})
+    res.send('Error, wrong validation token');
+});
+app.post('/webhook', (req, res) => {
+    const messagingEvents = req.body.entry[0].messaging;
 
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            decideMessage(sender, text);
-            continue
-        }
+    messagingEvents.forEach((event) => {
+        const sender = event.sender.id;
+
         if (event.postback) {
-        	let text = JSON.stringify(event.postback)
-        	decideMessage(sender, text);
-        	continue
-      }
-    }
-    res.sendStatus(200)
-})
+            const text = JSON.stringify(event.postback).substring(0, 200);
+            sendTextMessage(sender, 'Postback received: ' + text);
+        } else if (event.message && event.message.text) {
+            const text = event.message.text.trim().substring(0, 200);
 
-const token = "EAADPuY7AusgBAL5wNKMrokW32BgDCVESDwtXp0u4ytJ0gmxHB9LISBwGW2y6XFEi3oBgNFb0BqMbV89d80DUqfZBV4Ln4ttZBsKOg1alqDqaNA1Wlx4ZAFQOZBAj7iquLpmTcEEcDs6FriESRU0WTQ99TAECWB4UNWArcqGIhAZDZD"
+            if (text.toLowerCase() === 'generic') {
+                sendGenericMessage(sender);
+            } else {
+                sendTextMessage(sender, 'Text received, echo: ' + text);
+            }
+        }
+    });
 
-var popularTypesOfItems = {
-    invitations:  {
-        header: "Invitations",
-        subtitle: "Friends and Family Welcome!",
-        image_url: "http://scene7.targetimg1.com/is/image/Target/15406466?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
-        url: "http://www.target.com/c/baby-shower-party-supplies/-/N-4soei%7Cd_item_type_all%3Ainvitation%20packs?Sort=Featured&clearCategId=4soei&Nao=0",
-        button_title: "See Items Like"
-    },
-    favorBags: {
-        header: "Favor Bags",
-        subtitle: "Grab-and-go sweet treats",
-        image_url: "http://scene7.targetimg1.com/is/image/Target/50701956?wid=450&hei=450&fmt=pjpeg",
-        url: "http://www.target.com/c/baby-shower-party-supplies/-/N-4soei%7Cd_item_type_all%3AFavor%20Bag?Sort=Featured&clearCategId=4soei&Nao=0",
-        button_title: "See Items Like"
-    }
+    res.sendStatus(200);
+});
+
+function sendMessage (sender, message) {
+    request
+        .post('https://graph.facebook.com/v2.6/me/messages')
+        .query({access_token: pageToken})
+        .send({
+            recipient: {
+                id: sender
+            },
+            message: message
+        })
+        .end((err, res) => {
+            if (err) {
+                console.log('Error sending message: ', err);
+            } else if (res.body.error) {
+                console.log('Error: ', res.body.error);
+            }
+        });
 }
 
-var specificItems = {
-    sunscreens: {
-        sunscreen1: {
-            header: "Alba Botanica",
-            subtitle: "Keeping your skin youthful",
-            image_url: "http://scene7.targetimg1.com/is/image/Target/16872833?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
-            url: "http://www.target.com/p/alba-botanica-emollient-sunscreen-active-kids-clear-spray-spf-50-6-oz/-/A-16872833",
-            button_title_1: "See Item",
-            button_title_2: "Add to Cart"
-        },
-         sunscreen2: {
-            header: "Neutrogena Beach Defense",
-            subtitle: "Protecting your skin on the go!",
-            image_url: "http://scene7.targetimg1.com/is/image/Target/50787513?wid=450&hei=450&fmt=pjpeg",
-            url: "http://www.target.com/p/neutrogena-oh-joy-beach-defense-spray-sunscreen-broad-spectrum-spf-70-6-5-oz/-/A-50787513",
-            button_title_1: "See Item",
-            button_title_2: "Add to Cart"
-        },
-        sunscreen3: {
-            header: "Coppertone Kids",
-            subtitle: "Sun protection made easy!",
-            image_url: "http://scene7.targetimg1.com/is/image/Target/50584700?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
-            url: "http://www.target.com/p/coppertone-kids-sunscreen-continuous-spray-spf-50/-/A-50584700",
-            button_title_1: "See Item",
-            button_title_2: "Add to Cart"
-        }
-    },
-
-    infant_boy_clothing: {
-         clothing1: {
-            header: "Organically-Made Pajamas",
-            subtitle: "Keeping your baby comfy throughout the night",
-            image_url: "http://target.scene7.com/is/image/Target/51186963?wid=450&hei=450&fmt=pjpeg",
-            url: "http://www.target.com/p/lamaze-baby-waffle-footed-sleeper-red/-/A-51261899",
-            button_title_1: "See Item",
-            button_title_2: "Add to Cart"
-        },
-        clothing2: {
-            header: "Baby Boys' 4 Piece: Cat & Jack",
-            subtitle: "Providing style and comfort",
-            image_url: "http://target.scene7.com/is/image/Target/50897885?wid=450&hei=450&fmt=pjpeg",
-            url: "http://www.target.com/p/baby-boys-4-piece-tiger-set-baby-cat-jack-turquoise-white/-/A-51112241",
-            button_title_1: "See Item",
-            button_title_2: "Add to Cart"
-        },
-        clothing3: {
-            header: "Burt's Bees Infant 2 Pack",
-            subtitle: "Ready to rock and roll!",
-            image_url: "http://target.scene7.com/is/image/Target/50363846?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
-            url: "http://www.target.com/p/burt-s-bees-baby-infant-boys-2-pack-bodysuit-camo-striped/-/A-50363847",
-            button_title_1: "See Item",
-            button_title_2: "Add to Cart"
-        }
-
-    } 
+function sendTextMessage (sender, text) {
+    sendMessage(sender, {
+        text: text
+    });
 }
 
-
-function decideMessage(sender, text1) {
-	let text = text1.toLowerCase();
-	if (text.includes("baby shower") && text.includes("throwing")) {
-		sendTextMessage(sender, "Congrats! Do you need anything for your baby shower?");
-		send2TypesOfItems(sender, popularTypesOfItems.invitations, popularTypesOfItems.favorBags)
-	} else if (text.includes("add to cart")) {
-		sendTextMessage(sender, "Added to shopping cart!")
-	} else if (text.includes("no")) {
-		sendButtonMessage(sender, "I see it's summertime. Here are some things you might need.", "Sunscreen", "Flip Flops", "Sunglasses")
-	} else if (text.includes("sunscreen")) {
-		sendTextMessage(sender, "Here are some great deals on sunscreen!");
-	    send3SpecificItems(sender, specificItems.sunscreens.sunscreen1, specificItems.sunscreens.sunscreen2, specificItems.sunscreens.sunscreen3)
-	} else if (text.includes("gift")) {
-		sendButtonMessage(sender, "What is the occasion?", "Baby Shower", "Birthday", "Graduation");
-	} else if (text.includes("baby shower")) {
-		sendButtonMessage(sender, "What's your price range?", "$0 - $15", "$15 - $50", "$50 - $100")
-	} else if (text.includes("$")) {
-		sendButtonMessage(sender, "Here are some customer favorites!", "Clothes", "Stroller", "Toys")
-	} else if (text.includes("clothes")) {
-		sendTextMessage(sender, "Boy or girl?")
-	} else if (text.includes("boy")) {
-		send3SpecificItems(sender, specificItems.infant_boy_clothing.clothing1, specificItems.infant_boy_clothing.clothing2, specificItems.infant_boy_clothing.clothing3)
-	} else {
-		sendTextMessage(sender, "Can you ask again?");
-	}
-}
-
-function sendButtonMessage(sender, text, button1, button2, button3) {
-	let messageData = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":text,
-        "buttons":[
-          {
-            "type":"postback",
-            "title":button1,
-            "payload":button1.toLowerCase()
-          },
-          {
-            "type":"postback",
-            "title":button2,
-            "payload":button2.toLowerCase()
-          },
-          {
-            "type":"postback",
-            "title":button3,
-            "payload":button3.toLowerCase()
-          }
-        ]
-      }
-    } 
-}
-     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function send2TypesOfItems(sender, obj1, obj2) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": obj1.header,
-                    "subtitle": obj1.subtitle,
-                    "image_url": obj1.image_url,
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": obj1.url,
-                        "title": obj1.button_title
-                    }],
+function sendGenericMessage (sender) {
+    sendMessage(sender, {
+        attachment: {
+            type: 'template',
+            payload: {
+                template_type: 'generic',
+                elements: [{
+                    title: 'First card',
+                    subtitle: 'Element #1 of an hscroll',
+                    image_url: 'http://messengerdemo.parseapp.com/img/rift.png',
+                    buttons: [{
+                        type: 'web_url',
+                        url: 'https://www.messenger.com/',
+                        title: 'Web url'
+                    }, {
+                        type: 'postback',
+                        title: 'Postback',
+                        payload: 'Payload for first element in a generic bubble'
+                    }]
                 }, {
-                    "title": obj2.header,
-                    "subtitle": obj2.subtitle,
-                    "image_url": obj2.image_url,
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": obj2.url,
-                        "title": obj2.button_title
-                    }],
+                    title: 'Second card',
+                    subtitle: 'Element #2 of an hscroll',
+                    image_url: 'http://messengerdemo.parseapp.com/img/gearvr.png',
+                    buttons: [{
+                        type: 'postback',
+                        title: 'Postback',
+                        payload: 'Payload for second element in a generic bubble'
+                    }]
                 }]
             }
         }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+    });
 }
 
-function send3SpecificItems(sender, obj1, obj2, obj3) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": obj1.header,
-                    "subtitle": obj1.subtitle,
-                    "image_url": obj1.image_url,
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": obj1.url,
-                        "title": obj1.button_title_1
-                    }, {
-                        "type": "postback",
-                        "title": obj1.button_title_2,
-                        "payload": obj1.button_title_2.toLowerCase(),
-                    }],
-                }, {
-                    "title": obj2.header,
-                    "subtitle": obj2.subtitle,
-                    "image_url": obj2.image_url,
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": obj2.url,
-                        "title": obj2.button_title_1,
-                    }, {
-                    	"type": "postback",
-                        "title": obj2.button_title_2,
-                        "payload": obj2.button_title_2.toLowerCase(),
-                    }],
-                }, {
-                    "title": obj3.header,
-                    "subtitle": obj3.subtitle,
-                    "image_url": obj3.image_url,
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": obj3.url,
-                        "title": obj3.button_title_1,
-                    }, {
-                    	"type": "postback",
-                        "title": obj3.button_title_2,
-                        "payload": obj3.button_title_2.toLowerCase(),
-                    }],
-                }]
-            }
-        }
+app.post('/token', (req, res) => {
+    if (req.body.verifyToken === verifyToken) {
+        pageToken = req.body.token;
+        return res.sendStatus(200);
     }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+    res.sendStatus(403);
+});
+app.get('/token', (req, res) => {
+    if (req.body.verifyToken === verifyToken) {
+        return res.send({token: pageToken});
+    }
+    res.sendStatus(403);
+});
 
-
-// Spin up the server
-//var https = require('https');
-//https.createServer(options, app).listen(8443, function(){
-//   console.log('server is up and running');
-//});
-
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+ https.createServer({
+      key: fs.readFileSync(privkey),
+      cert: fs.readFileSync(cert),
+      ca: fs.readFileSync(chain)
+    }, app).listen(55555, function () {
+  console.log('App is ready on port 55555');
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
